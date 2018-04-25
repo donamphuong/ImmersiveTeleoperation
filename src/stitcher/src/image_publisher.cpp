0576af12a0cab96821aacd51c055eef8348bbcf5
Phuong <donamphuong.98@gmail.com>
2018-04-25 11:02:00 +0100

    live window of two cameras

--- src/stitcher/src/image_publisher.cpp
+++ src/stitcher/src/image_publisher.cpp
@@ -15,13 +15,14 @@ std::string to_string(int x) {
   return stream.str();
 }
 
-void save_frame(VideoCapture cap, std::vector<Mat> images) {
+void save_frame(VideoCapture cap, std::vector<Mat> images, int view) {
   Mat frame;
   sensor_msgs::ImagePtr msg;
   cap >> frame;
   //Check if the grabbed frame is actually full with some content
   if (!frame.empty()) {
     images.push_back(frame);
+    imshow("view" + to_string(view), frame);
     cv::waitKey(1);
   }
 }
@@ -38,6 +39,10 @@ int main(int argc, char** argv) {
   image_transport::ImageTransport it(nh);
   int num_cam = atoi(argv[1]);
 
+  cv::namedWindow("view1");
+  cv::startWindowThread();
+  cv::namedWindow("view2");
+  cv::startWindowThread();
   ros::Rate loop_rate(5);
   std::cout << "Image Publisher running";
 
@@ -47,7 +52,7 @@ int main(int argc, char** argv) {
   cv::VideoCapture cap[num_cam];
 
   for (int video_source = 1; video_source < num_cam + 1; video_source++) {
-    cap[video_source-1].open(video_source-1);
+    cap[video_source-1].open(video_source);
 
     //Check if video device can be opened with the given index
     std::cout << cap[video_source-1].isOpened() << "\n";
@@ -57,22 +62,23 @@ int main(int argc, char** argv) {
   std::vector<Mat> images;
   while (nh.ok()) {
     for (int i = 0; i < num_cam; i++) {
-      save_frame(cap[i], images);
+      save_frame(cap[i], images, i+1);
     }
 
-    Mat pano;
-    //Converting ROS image message to an OpenCV image with BGR pixel encoding, then show it in a display window
-    Ptr<Stitcher> stitcher = Stitcher::create(Stitcher::PANORAMA, true);
-    Stitcher::Status status = stitcher->stitch(images, pano);
-
-    if (status != Stitcher::OK) {
-      std::cout << "Can't stitch images, error code = " << int(status) << std::endl;
-      return 1;
-    }
-
-    std::cout << "Images are Stitched";
-    sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", pano).toImageMsg();
-    pub.publish(msg);
+    // Mat pano;
+    // //Converting ROS image message to an OpenCV image with BGR pixel encoding, then show it in a display window
+    // Ptr<Stitcher> stitcher = Stitcher::create(Stitcher::PANORAMA, true);
+    // Stitcher::Status status = stitcher->stitch(images, pano);
+    //
+    // if (status != Stitcher::OK) {
+    //   std::cout << "Can't stitch images, error code = " << int(status) << std::endl;
+    //   waitKey(1);
+    //   // return 1;
+    // }
+    //
+    // std::cout << "Images are Stitched";
+    // sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", pano).toImageMsg();
+    // pub.publish(msg);
 
     ros::spinOnce();
     loop_rate.sleep();
